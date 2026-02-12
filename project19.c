7de9ee552e0be351f983d04adb30271c64455fb9
Fidel Castro <fidelcastro4080@gmail.com>
2026-02-12 07:16:49 +0300

    ...

---
+++ project19.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    float radius = 5;
+    float area = PI * radius * radius;  // PI is replaced with 3.14159
+    printf("Area = %f\n", area);
+    return 0;
+}
